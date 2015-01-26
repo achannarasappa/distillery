ba71c7e5831b6bcace8d3b65f844e5b6e8ed8ef9
Ani <ani@channarasappa.com>
2015-01-25 23:07:40 -0500

    Added ability to set table options from cli

--- bin/distillery.js
+++ bin/distillery.js
@@ -3,12 +3,16 @@
 var program = require('commander'),
     coercion = require('commander-coercions'),
     info = require('../package.json');
-
+function collect(val, memo) {
+  memo.push(val);
+  return memo;
+}
 program
   .version(info.version, '-v, --version');
 
-program.command('ignite')
+program.command('ignite <file>')
   .option('-p, --parameters [value]', 'parameter to pass to the still', coercion.toListFromMultipleInvocations, [])
+  .option('-o, --opts [value]', 'set distillery options', coercion.toListFromMultipleInvocations, [])
   .action(require('./distillery-ignite'));
 
 
